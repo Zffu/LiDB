64e569bcd76ee401bf9783cfc7103b8dfc879061
Zffu <radi0o0@outlook.com>
2024-10-14 00:57:21 +0200

    feat: updated database server implementation

--- server/src/main/java/net/zffu/lidb/server/database/LiDB.java
+++ server/src/main/java/net/zffu/lidb/server/database/LiDB.java
@@ -2,28 +2,40 @@ package net.zffu.lidb.server.database;
 
 import net.zffu.lidb.database.LIDatabase;
 import net.zffu.lidb.database.table.LITable;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
  * The server implementation of the {@link net.zffu.lidb.database.LIDatabase}.
  * @since 1.0.0
  */
-public class LiDB extends LIDatabase {
+public class LiDB implements LIDatabase {
+
+    private final LITable<?>[] tables;
 
     /**
-     * <p>Creates a new {@link LiDB} with the provided tables.</p>
+     * <p>Creates a {@link LiDB} with the provided tables.</p>
      * @param tables the tables.
      */
-    public LiDB(@Nullable LITable<?>[] tables) {
+    public LiDB(LITable<?>[] tables) {
         this.tables = tables;
     }
 
     /**
-     * <p>Creates a new {@link LiDB} with the provided table amount.</p>
+     * <p>Creates a {@link LiDB} with the provided table amount.</p>
      * @param tables the table amount.
      */
     public LiDB(int tables) {
-        this.tables = new LITable[tables];
+        this.tables = new LITable<?>[tables];
     }
 
+    @Override
+    public @NotNull LITable<?>[] getTables() {
+        return this.tables;
+    }
+
+    @Override
+    public @Nullable <T> LITable<T> getTable(int index, @NotNull Class<T> clazz) {
+        return (LITable<T>) this.tables[index];
+    }
 }
