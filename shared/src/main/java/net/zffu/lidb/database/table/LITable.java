18d69d580cf652a1ee882ca34cc097fb51c7bdf4
Zffu <radi0o0@outlook.com>
2024-10-14 00:31:30 +0200

    feat: added entries inside the table

--- shared/src/main/java/net/zffu/lidb/database/table/LITable.java
+++ shared/src/main/java/net/zffu/lidb/database/table/LITable.java
@@ -3,6 +3,7 @@ package net.zffu.lidb.database.table;
 
 import net.zffu.lidb.types.DataType;
 
+import java.util.HashMap;
 /**
  * <p>A table in an {@link net.zffu.lidb.database.LIDatabase}.</p>
  * @param <P> the primary key type in thr table.
@@ -14,6 +15,11 @@ public class LITable<P> {
      * <p>The primary key data type.</p>
      */
     private DataType<P> primaryKey;
+
+    /**
+     * <p>The entries contained in the table.</p>
+     */
+    private HashMap<P, Object[]> entries = new HashMap<>();
 
     /**
      * <p>Constructs a new {@link LITable}.</p>
