e413cbc2d33f7f77d976643f59a322ca54818fc4
Zffu <radi0o0@outlook.com>
2024-10-14 00:51:56 +0200

    feat: made LITable into an interface

--- shared/src/main/java/net/zffu/lidb/database/table/LITable.java
+++ shared/src/main/java/net/zffu/lidb/database/table/LITable.java
@@ -1,25 +1,19 @@
 package net.zffu.lidb.database.table;
 
-
 import net.zffu.lidb.types.DataType;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.HashMap;
 /**
  * <p>A table in an {@link net.zffu.lidb.database.LIDatabase}.</p>
  * @param <P> the primary key type in thr table.
  * @since 1.0.0
  */
-public class LITable<P> {
+public interface LITable<P> {
 
     /**
-     * <p>The primary key data type.</p>
+     * <p>Gets the primary key data type of the table.</p>
+     * @return the data type of the table.
      */
-    private @NotNull DataType<P> primaryKey;
-
-    /**
-     * <p>The entries contained in the table.</p>
-     */
-    private HashMap<P, Object[]> entries = new HashMap<>();
+    @NotNull DataType<P> getPrimaryDataType();
 
 }
