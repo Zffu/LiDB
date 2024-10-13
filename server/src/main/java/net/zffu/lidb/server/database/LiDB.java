85591499b8c731a3fc0d272e9198a922136fc90e
Zffu <radi0o0@outlook.com>
2024-10-14 00:44:11 +0200

    feat: added server implementation db

---
+++ server/src/main/java/net/zffu/lidb/server/database/LiDB.java
@@ -0,0 +1,28 @@
+package net.zffu.lidb.server.database;
+
+import net.zffu.lidb.database.LIDatabase;
+import net.zffu.lidb.database.table.LITable;
+
+/**
+ * The server implementation of the {@link net.zffu.lidb.database.LIDatabase}.
+ * @since 1.0.0
+ */
+public class LiDB extends LIDatabase {
+
+    /**
+     * <p>Creates a new {@link LiDB} with the provided tables.</p>
+     * @param tables the tables.
+     */
+    public LiDB(LITable<?>[] tables) {
+        this.tables = tables;
+    }
+
+    /**
+     * <p>Creates a new {@link LiDB} with the provided table amount.</p>
+     * @param tables the table amount.
+     */
+    public LiDB(int tables) {
+        this.tables = new LITable[tables];
+    }
+
+}
