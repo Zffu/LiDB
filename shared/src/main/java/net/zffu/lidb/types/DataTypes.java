2f70f1905a96a8f525047482e0ff7d6bb254696b
Zffu <radi0o0@outlook.com>
2024-10-14 00:25:38 +0200

    feat: added data types registry

---
+++ shared/src/main/java/net/zffu/lidb/types/DataTypes.java
@@ -0,0 +1,13 @@
+package net.zffu.lidb.types;
+
+/**
+ * <p>Stores the different data types instances.</p>
+ * <p><b>NOTE: </b>This isn't an enum as enum cannot support different data types.</p>
+ * @since 1.0.0
+ */
+public class DataTypes {
+
+
+
+
+}
