41013062ec4de91207ec1b097ae89f512ee50b22
Zffu <radi0o0@outlook.com>
2024-10-14 00:28:15 +0200

    feat: added data types inside table

--- shared/src/main/java/net/zffu/lidb/database/table/LITable.java
+++ shared/src/main/java/net/zffu/lidb/database/table/LITable.java
@@ -1,9 +1,26 @@
 package net.zffu.lidb.database.table;
 
 
+import net.zffu.lidb.types.DataType;
+
 /**
  * <p>A table in an {@link net.zffu.lidb.database.LIDatabase}.</p>
+ * @param <P> the primary key type in thr table.
  * @since 1.0.0
  */
-public class LITable {
+public class LITable<P> {
+
+    /**
+     * <p>The primary key data type.</p>
+     */
+    private DataType<P> primaryKey;
+
+    /**
+     * <p>Constructs a new {@link LITable}.</p>
+     * @param primaryKey the primary key data type, is used for conversion.
+     */
+    public LITable(DataType<P> primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+
 }
