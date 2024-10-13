71786b9da30b75b1d315b01c1dbc81ed9b0e4b21
Zffu <radi0o0@outlook.com>
2024-10-14 00:50:38 +0200

    feat: made LIDatabase into an interface

--- shared/src/main/java/net/zffu/lidb/database/LIDatabase.java
+++ shared/src/main/java/net/zffu/lidb/database/LIDatabase.java
@@ -1,13 +1,27 @@
 package net.zffu.lidb.database;
 
 import net.zffu.lidb.database.table.LITable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * <p>A database instance represented in the LiDB library.</p>
  * @since 1.0.0
  */
-public class LIDatabase {
+public interface LIDatabase {
 
-    protected LITable[] tables;
+    /**
+     * <p>Gets all of the tables contained in the database.</p>
+     * @return the tables as an array.
+     */
+    @NotNull LITable<?>[] getTables();
+
+    /**
+     * <p>Gets the table located at the provided index.</p>
+     * @param index the index.
+     * @param <T> the data type of the primary key of said table.
+     * @return the table.
+     */
+    @Nullable <T> LITable<T> getTable(int index, @NotNull Class<T> clazz);
 
 }
