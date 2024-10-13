0de93c4922dbcbb3242cecf114e99dcf60e3f993
Zffu <radi0o0@outlook.com>
2024-10-14 00:16:13 +0200

    feat: added tables inside database

--- shared/src/main/java/net/zffu/lidb/database/LIDatabase.java
+++ shared/src/main/java/net/zffu/lidb/database/LIDatabase.java
@@ -1,10 +1,21 @@
 package net.zffu.lidb.database;
 
+import net.zffu.lidb.database.table.LITable;
+
 /**
  * <p>A database instance represented in the LiDB library.</p>
  * @since 1.0.0
  */
 public class LIDatabase {
 
+    private LITable[] tables;
+
+    /**
+     * <p>Creates a new {@link LIDatabase} with predefined tables.</p>
+     * @param tables the tables.
+     */
+    public LIDatabase(LITable[] tables) {
+        this.tables = tables;
+    }
 
 }
